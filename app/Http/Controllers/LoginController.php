e558123208aa8329d8ee783e27a20adb6da7e043
Mayra Lopez <74613812+MayraLizLopez@users.noreply.github.com>
2021-03-30 10:24:47 -0600

    se agrego un seeder para usuarios, y se agrego un controlador para login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\Voluntario;
+
+class LoginController extends Controller
+{
+    //
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $usuarios = DB::select('SELECT * FROM usuarios');
+       foreach ($usuarios as $usuario) {
+
+        //dd($usuario);
+         $nombre = $usuario->email;
+         $nombreC = $usuario->nombre ." ". $usuario->ape_pat." ". $usuario->ape_mat;
+         if($nombre == $request->email && $usuario->password == $request->password){
+           session(['sesionUsu' => $nombreC]);
+           //Cambiar por la vista de logín exitoso
+           return view('templates.default');
+
+         }
+       }
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Correo y/o contraseña incorrectas.',
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
---
+++ database/seeders/UsuariosSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsuariosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('usuarios')->insert([
+            'nombre' => 'Jaime',
+            'ape_pat' => 'León',
+            'ape_mat' => 'Murrieta',
+            'id_insti' => 1,
+            'cargo' => 'Director',
+            'rol' => 'Administrador General',
+            'tel' => '123456789',
+            'email' => 'jaime@gmail.com',
+            'password' => Hash::make('1234'),
+            'activo' => true,
+        ]);
+
+        DB::table('usuarios')->insert([
+            'nombre' => 'Mayra',
+            'ape_pat' => 'Lopez',
+            'ape_mat' => 'Barrios',
+            'id_insti' => 2,
+            'cargo' => 'Director',
+            'rol' => 'Administrador',
+            'tel' => '123456788',
+            'email' => 'mayra@gmail.com',
+            'password' => Hash::make('1234'),
+            'activo' => true,
+        ]);
+
+        DB::table('usuarios')->insert([
+            'nombre' => 'Arturo',
+            'ape_pat' => 'Bravo',
+            'ape_mat' => 'Baéz',
+            'id_insti' => 2,
+            'cargo' => 'Encargado',
+            'rol' => 'Enlace de institución',
+            'tel' => '123456888',
+            'email' => 'arturo@gmail.com',
+            'password' => Hash::make('1234'),
+            'activo' => true,
+        ]);
+    }
+}
