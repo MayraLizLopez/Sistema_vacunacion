54c274ffbef1e270ffaa6916e3379ff7332f12a5
Mayra Lopez <74613812+MayraLizLopez@users.noreply.github.com>
2021-04-06 13:25:46 -0500

    se modifico vista registro voluntarios

---
+++ resources/views/layout_gray.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('tittle')</title>
+        <link rel="stylesheet" href="{{ url("../resources/css/bootstrap.min.css") }}">
+        <link rel="stylesheet" href="{{ url("../resources/css/site.css") }}">
+        <link rel="stylesheet" href="{{ url("../resources/css/sb-admin-2.min.css") }}">
+        <link rel="stylesheet" href="{{ url("../resources/css/sweetalert2.min.css") }}">    
+        <link href="{{ url("../resources/css/all.min.css") }}" rel="stylesheet">  
+    </head>
+    <body style="background-color: gray;">
+        @yield('content')
+        <script src="{{ url("../resources/js/jquery.slim.min.js") }}"></script>
+        <script src="{{ url("../resources/js/bootstrap.min.js") }}"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="{{ url("../resources/js/jquery.easing.min.js") }}"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="{{ url("../resources/js/sb-admin-2.min.js") }}"></script>
+
+        <!-- Page level plugins -->
+        <script src="{{ url("../resources/js/Chart.min.js") }}"></script>
+
+        <script src="{{ url("../resources/js/fontawesome.min.js") }}"></script>
+        <script src="{{ url("../resources/js/all.min.js") }}"></script>
+
+        <!-- Page level custom scripts -->
+        <script src="{{ url("../resources/js/chart-area-demo.js") }}"></script>
+        <script src="{{ url("../resources/js/chart-pie-demo.js") }}"></script>
+        <script src="{{ url("../resources/js/fontawesome.min.js") }}"></script>
+        <script src="{{ url("../resources/js/sweetalert2.min.js") }}"></script>
+        @yield('scripts')
+    </body>
+</html>
--- resources/views/volunteers/registration.blade.php
+++ resources/views/volunteers/registration.blade.php
@@ -1,132 +1,123 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('tittle')</title>
-        <link rel="stylesheet" href="{{ url("../resources/css/bootstrap.min.css") }}">
-        <link rel="stylesheet" href="{{ url("../resources/css/site.css") }}">
-        <link rel="stylesheet" href="{{ url("../resources/css/sb-admin-2.min.css") }}">
-        <link rel="stylesheet" href="{{ url("../resources/css/sweetalert2.min.css") }}">    
-        <link href="{{ url("../resources/css/all.min.css") }}" rel="stylesheet">  
-    </head>
-    <body style="width 100%; background-color:gray;">
-    <div class="container" style="width: 100%;">
-        <div class="card shadow mb-4"  style="margin-top: 30px;">
-            <div class="card-body">
-                <div class="content">
-                    <div class="col-sm-12">
-                        <div class="row row justify-content-center">
-                            <div class="col-sm-12">
-                                <h1 style="margin-top: 30px; margin-bottom: 30px;">Registro de voluntarios</h1>
-                            </div>
+@extends('layout_gray')
+@section('css')
+    <link href="{{ url("../resources/css/bootstrap-table.min.css") }}" rel="stylesheet" type="text/css">
+@endsection
+@section('title', 'Registro de Voluntarios')
+@section('content')
+<div class="container" style="width: 100%;">
+    <div class="card shadow mb-4"  style="margin-top: 30px;">
+        <div class="card-body">
+            <div class="content">
+                <div class="col-sm-12">
+                    <div class="row row justify-content-center">
+                        <div class="col-sm-12">
+                            <h1 style="margin-top: 30px; margin-bottom: 30px;">Registro de voluntarios</h1>
                         </div>
-                        <div class="row justify-content-center">
-                            <div class="col-sm-12">
-                                <form method="POST" action="{{url("/voluntario/store")}}">
-                                    @if(Session::get('success'))
-                                        <div class="alert alert-success">
-                                            {{ Session::get('success') }}
-                                        </div>
-                                    @endif
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-sm-12">
+                            <form method="POST" action="{{url("/voluntario/store")}}">
+                                @if(Session::get('success'))
+                                    <div class="alert alert-success">
+                                        {{ Session::get('success') }}
+                                    </div>
+                                @endif
 
-                                    @if(Session::get('fail'))
-                                        <div class="alert alert-danger">
-                                            {{ Session::get('fail') }}
-                                        </div>
-                                    @endif 
+                                @if(Session::get('fail'))
+                                    <div class="alert alert-danger">
+                                        {{ Session::get('fail') }}
+                                    </div>
+                                @endif 
 
-                                    {{ csrf_field() }}
+                                {{ csrf_field() }}
 
-                                    <div class="card shadow mb-4">
-                                        <div class="card-header py-3">
-                                            <h5 class="m-0 font-weight-bold text-primary">Ingrese sus datos personales</h5>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="content">
-                                                <div class="row">
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label for="nameVoluntary">Nombre(s)</label>
-                                                            <input type="text" class="form-control" id="nameVoluntary" name="nombre" placeholder="Nombre"/>
-                                                            <span class="text-danger">@error('nombre'){{ 'Ingrese su nombre o nombres' }} @enderror </span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label for="paternalSurnameVoluntary">Apellido Paterno</label>
-                                                            <input type="text" class="form-control" id="paternalSurnameVoluntary" name="ape_pat" placeholder="Apellido Paterno"/>
-                                                            <span class="text-danger">@error('ape_pat'){{ 'Ingrese su apellido paterno' }} @enderror </span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label for="paternalSurnameVoluntary">Apellido Materno</label>
-                                                            <input type="text" class="form-control" id="paternalSurnameVoluntary" name="ape_mat" placeholder="Apellido Materno"/>
-                                                        </div>
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h5 class="m-0 font-weight-bold text-primary">Ingrese sus datos personales</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label for="nameVoluntary">Nombre(s)</label>
+                                                        <input type="text" class="form-control" id="nameVoluntary" name="nombre" placeholder="Nombre"/>
+                                                        <span class="text-danger">@error('nombre'){{ 'Ingrese su nombre o nombres' }} @enderror </span>
                                                     </div>
                                                 </div>
-
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="nameVoluntary">Correo electrónico</label>
-                                                            <input type="text" class="form-control" id="nameVoluntary" name="email" placeholder="Correo"/>
-                                                            <span class="text-danger">@error('email'){{ 'Ingrese un email correcto o no registrado en la plataforma' }} @enderror </span>
-                                                        </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label for="paternalSurnameVoluntary">Apellido Paterno</label>
+                                                        <input type="text" class="form-control" id="paternalSurnameVoluntary" name="ape_pat" placeholder="Apellido Paterno"/>
+                                                        <span class="text-danger">@error('ape_pat'){{ 'Ingrese su apellido paterno' }} @enderror </span>
                                                     </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="paternalSurnameVoluntary">Teléfono / Celular</label>
-                                                            <input type="text" class="form-control" id="phoneVoluntary" name="tel" placeholder="Número celular o fijo">
-                                                            <span class="text-danger">@error('tel'){{ 'Ingrese un número telefónico' }} @enderror </span>
-                                                        </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label for="paternalSurnameVoluntary">Apellido Materno</label>
+                                                        <input type="text" class="form-control" id="paternalSurnameVoluntary" name="ape_mat" placeholder="Apellido Materno"/>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="nameVoluntary">Correo electrónico</label>
+                                                        <input type="text" class="form-control" id="nameVoluntary" name="email" placeholder="Correo"/>
+                                                        <span class="text-danger">@error('email'){{ 'Ingrese un email correcto o no registrado en la plataforma' }} @enderror </span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="paternalSurnameVoluntary">Teléfono / Celular</label>
+                                                        <input type="text" class="form-control" id="phoneVoluntary" name="tel" placeholder="Número celular o fijo">
+                                                        <span class="text-danger">@error('tel'){{ 'Ingrese un número telefónico' }} @enderror </span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="card shadow mb-4">
-                                        <div class="card-header py-3">
-                                            <h5 class="m-0 font-weight-bold text-primary">Ingrese los datos de la institución</h5>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="content">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="instututionVoluntary">Institución</label>
-                                                            <select class="form-control" id="instututionVoluntary" name="id_insti">
-                                                            <span class="text-danger">@error('id_insti'){{ 'Seleccione una institución' }} @enderror </span>
-                                                            </select>
-                                                        </div>
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h5 class="m-0 font-weight-bold text-primary">Ingrese los datos de la institución</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="instututionVoluntary">Institución</label>
+                                                        <select class="form-control" id="instututionVoluntary" name="id_insti">
+                                                        <span class="text-danger">@error('id_insti'){{ 'Seleccione una institución' }} @enderror </span>
+                                                        </select>
                                                     </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label for="townVoluntary">Municipio</label>
-                                                            <select class="form-control" id="townVoluntary" name="id_municipio">
-                                                            </select>
-                                                            <span class="text-danger">@error('id_municipio'){{ 'Seleccione un municipio' }} @enderror </span>
-                                                        </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="townVoluntary">Municipio</label>
+                                                        <select class="form-control" id="townVoluntary" name="id_municipio">
+                                                        </select>
+                                                        <span class="text-danger">@error('id_municipio'){{ 'Seleccione un municipio' }} @enderror </span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary" id="sendFormVoluntaries">Enviar</button>
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal" ><a style="color:white;" href="{{route('home')}}">Cancelar</a></button>
-                                </form>
-                            </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary" id="sendFormVoluntaries">Enviar</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal" ><a style="color:white;" href="{{route('home')}}">Cancelar</a></button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
-</html>
+</div>
+@endsection
 @section('scripts')
     <script type="text/javascript">
         let municipios = @json($municipios);
