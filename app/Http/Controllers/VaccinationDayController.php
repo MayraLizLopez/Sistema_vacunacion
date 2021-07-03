e7ae0224293cd7d8dcf5e153de676f3ad69a35e4
Jaime Javier León Murrieta <jaimeleonm@outlook.com>
2021-07-03 18:13:31 -0500

    cambio leve

--- app/Http/Controllers/VaccinationDayController.php
+++ app/Http/Controllers/VaccinationDayController.php
@@ -659,7 +659,7 @@ class VaccinationDayController extends Controller
                 } else if($j <=990){
                     Mail::to($voluntario->email)->send(new ConfirmJornada($data, 'voluntariado3.jalisco@gmail.com'));
                 } else if($j <=1485){
-                    Mail::to($voluntario->email)->send(new ConfirmJornada($data, 'voluntariado4.jalisco@gmail.com'));
+                    Mail::to($voluntario->email)->send(new ConfirmJornada($data, 'voluntariado2.jalisco@gmail.com'));
                 }
              
                 for($k = 0; $k < count($sedes); $k++){
